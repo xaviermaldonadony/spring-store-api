f5c4a117dc20a2b3e5d2a7ec595de7a0ddf22090
Xavier <Xaviermaldonadony@hotmail.com>
2025-09-19 02:50:29 -0400

    implemented CartController and Dtos

--- src/main/java/com/example/store/entities/CartItem.java
+++ src/main/java/com/example/store/entities/CartItem.java
@@ -1,12 +1,6 @@
 package com.example.store.entities;
 
-import jakarta.persistence.Id;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Column;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.JoinColumn;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
