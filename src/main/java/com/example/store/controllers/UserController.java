68ebc71b8b8cf8ad2d451184a1a30521ac633c88
Xavier <Xaviermaldonadony@hotmail.com>
2025-09-18 21:23:56 -0400

    registerUser, checks for duplicate emails

--- src/main/java/com/example/store/controllers/UserController.java
+++ src/main/java/com/example/store/controllers/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import com.example.store.repositories.UserRepository;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.Map;
 import java.util.Set;
 
 @AllArgsConstructor
@@ -52,9 +53,15 @@ public class UserController {
     }
 
     @PostMapping
-    public ResponseEntity<UserDto> createUser(
+    public ResponseEntity<?> registerUser(
             @Valid @RequestBody RegisterUserRequest request,
             UriComponentsBuilder uriBuilder) {
+
+        if (userRepository.existsByEmail(request.getEmail())){
+            return ResponseEntity.badRequest().body(
+                    Map.of("email", "Email is already registered")
+            );
+        }
 
         var user = userMapper.toEntity(request);
         userRepository.save(user);
