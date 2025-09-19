386a429fef5aa613d13a1f0d55796866e8c567ae
Xavier <Xaviermaldonadony@hotmail.com>
2025-09-18 20:57:34 -0400

    moved exceptionHandler to a class

--- src/main/java/com/example/store/controllers/UserController.java
+++ src/main/java/com/example/store/controllers/UserController.java
@@ -117,15 +117,4 @@ public class UserController {
 
         return ResponseEntity.noContent().build();
     }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> handleValidationErrors(MethodArgumentNotValidException exception) {
-        var errors = new HashMap<String, String>();
-
-        exception.getBindingResult().getFieldErrors().forEach(error -> {
-            errors.put(error.getField(), error.getDefaultMessage());
-        });
-
-        return ResponseEntity.badRequest().body(errors);
-    }
 }
