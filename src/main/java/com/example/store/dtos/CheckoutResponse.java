a73865248628b5dd31db20d708680cf087665d72
Xavier <Xaviermaldonadony@hotmail.com>
2025-09-27 21:20:36 -0400

    implemented stripe checkout session

--- src/main/java/com/example/store/dtos/CheckoutResponse.java
+++ src/main/java/com/example/store/dtos/CheckoutResponse.java
@@ -5,8 +5,10 @@ import lombok.Data;
 @Data
 public class CheckoutResponse {
     private Long orderId;
+    private String checkoutUrl;
 
-    public CheckoutResponse(Long id) {
+    public CheckoutResponse(Long id, String checkoutUrl) {
         this.orderId = id;
+        this.checkoutUrl = checkoutUrl;
     }
 }
--- src/main/java/com/example/store/services/CheckoutService.java
+++ src/main/java/com/example/store/services/CheckoutService.java
@@ -7,10 +7,14 @@ import com.example.store.exceptions.CartEmptyException;
 import com.example.store.exceptions.CartNotFoundException;
 import com.example.store.repositories.CartRepository;
 import com.example.store.repositories.OrderRepository;
-import lombok.AllArgsConstructor;
+import com.stripe.exception.StripeException;
+import com.stripe.model.checkout.Session;
+import com.stripe.param.checkout.SessionCreateParams;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Service
 public class CheckoutService {
     private final CartRepository cartRepository;
@@ -18,8 +22,10 @@ public class CheckoutService {
     private final AuthService authService;
     private final CartService cartService;
 
+    @Value("${websiteUrl}")
+    private String websiteUrl;
 
-    public CheckoutResponse checkout(CheckoutRequest request){
+    public CheckoutResponse checkout(CheckoutRequest request) throws StripeException {
         var cart = cartRepository .getCartWithItems(request.getCartId()).orElse(null);
 
         if (cart == null) {
@@ -32,8 +38,35 @@ public class CheckoutService {
         var order =  Order.fromCart(cart, authService.getCurrentUser());
 
         orderRepository.save(order);
+        // Create a checkout session
+        var builder = SessionCreateParams.builder()
+            .setMode(SessionCreateParams.Mode.PAYMENT)
+            .setSuccessUrl(websiteUrl + "/checkout-success?orderId=" + order.getId())
+            .setCancelUrl(websiteUrl + "/checkout-cancel");
+
+        order.getItems().forEach(item -> {
+            var lineItem = SessionCreateParams.LineItem.builder()
+                    .setQuantity(Long.valueOf(item.getQuantity()))
+                    .setPriceData(
+                       SessionCreateParams.LineItem.PriceData.builder()
+                           .setCurrency("usd")
+                           .setUnitAmountDecimal(item.getUnitPrice())
+                           .setProductData(
+                               SessionCreateParams.LineItem.PriceData.ProductData.builder()
+                                   .setName(item.getProduct().getName())
+                                   .build()
+                           )
+                           .build()
+                    )
+                    .build();
+            builder.addLineItem(lineItem);
+        });
+
+        // return SessionCreateParams
+        var session = Session.create(builder.build());
+
         cartService.clearCart(cart.getId());
 
-        return new CheckoutResponse(order.getId());
+        return new CheckoutResponse(order.getId(), session.getUrl());
     }
 }
